8ae861079c76811d9cb7c0c25840d52761be7506
Kush_Shah <35930270+shahkv95@users.noreply.github.com>
2020-10-23 06:06:05 +0530

    Add files via upload

---
+++ javaServlet/src/Hello.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Hello() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h3>Hello Servlet</h3>");
+		out.print("</body></html>");
+	}
+
+}
